5dce3f90839beb9ece51add749e8ad1d3d2f6946
Mai Van Manh <mvmanh@gmail.com>
2023-03-20 21:55:50 +0700

    update main.js

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ console.clear();
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Great! It is working right now! Okay?"));
+app.get("/", (req, res) =>
+  res.send("Great! It is working right now! Okay????")
+);
 app.get("/about", (req, res) => res.send("About Page"));
 app.get("/contact", (req, res) => res.send("Contact Page"));
 app.listen(8080, () =>
